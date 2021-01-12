1f5616d2e56de8ae84c590b6ec8745b2861d968d
Andrea Tredrea <andrea.tredrea@gmail.com>
2021-01-12 12:53:45 +0100

    creating cards and fetching fake API

--- src/App.js
+++ src/App.js
@@ -5,19 +5,38 @@ import Flat from './components/flat'
 
 
 class App extends Component {
-  render() {
-    const flat = {
-      "id": 148,
-      "name": "Trendy Apt in Buttes Montmartre",
-      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
-      "price": 200,
-      "priceCurrency": "EUR",
-      "lat": 48.885707,
-      "lng": 2.343543
+  constructor (props) {
+    super (props);
+    this.state = {
+      flats: []
     };
+  }
+
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => { 
+        this.setState({
+          flats: data
+        });
+    })
+  }
+
+  render() {
     return (
-      <div>
-        <Flat flat={flat} />
+      <div className="app">
+        <div className="main">
+          <div className="search">
+          </div>
+          <div className="flats">
+            {this.state.flats.map(function(flat) {
+              return <Flat flat={flat} />
+            })}
+          </div>
+        </div>
+        <div className="map">
+        </div>
       </div>
     );
   }
